668102dc1198789b07a41d5795bef4ea546887a7
Daniel Freitas <git@scrlkx.com>
2025-12-13 14:27:14 -0300

    source code

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [icon()],
+});
